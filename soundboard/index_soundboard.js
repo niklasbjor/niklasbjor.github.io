0d5b6e221ec06e1dee0f50a90de426b429208dc5
Niklas Björklund <niklas.bjor@gmail.com>
2020-05-15 20:39:37 +0200

    extract js to separate file

---
+++ soundboard/index_soundboard.js
@@ -0,0 +1,6 @@
+let dasAlTeLoate = new Audio("soundboard/resources/sounds/dasalteloate.mp3");
+let oekNiWaar = new Audio("soundboard/resources/sounds/oekniwaar.mp3");
+let eyKalmHeZalm = new Audio("soundboard/resources/sounds/eykalmhezalm.mp3");
+let munt = new Audio("soundboard/resources/sounds/munt.mp3");
+let ochMennekeToch = new Audio("soundboard/resources/sounds/ochmenneketoch.mp3");
+let wegSfeer = new Audio("soundboard/resources/sounds/wegsfeer.mp3");
