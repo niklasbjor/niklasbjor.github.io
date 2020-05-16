a081b0637bc33a388f4b62ddc8e56334b83410fb
Niklas Björklund <niklas.bjor@gmail.com>
2020-05-16 10:21:46 +0200

    remove duplication

--- soundboard/index_soundboard.js
+++ soundboard/index_soundboard.js
@@ -1,27 +1,17 @@
-let dasAlTeLoate = new Audio("soundboard/resources/sounds/dasalteloate.mp3");
-let oekNiWaar = new Audio("soundboard/resources/sounds/oekniwaar.mp3");
-let eyKalmHeZalm = new Audio("soundboard/resources/sounds/eykalmhezalm.mp3");
-let munt = new Audio("soundboard/resources/sounds/munt.mp3");
-let ochMennekeToch = new Audio("soundboard/resources/sounds/ochmenneketoch.mp3");
-let wegSfeer = new Audio("soundboard/resources/sounds/wegsfeer.mp3");
-let gertruuuude = new Audio("soundboard/resources/sounds/gertruuuude.mp3");
-let goeiendaag = new Audio("soundboard/resources/sounds/goeiendaag.mp3");
-let kustNaMenKlte = new Audio("soundboard/resources/sounds/kustnamenkl.mp3");
-let lapjepap = new Audio("soundboard/resources/sounds/lapjepap.mp3");
-let lelijkIsNiks = new Audio("soundboard/resources/sounds/lelijkisniks.mp3");
-let ochgodjemenas = new Audio("soundboard/resources/sounds/ochgodjemenas.mp3");
+let soundTuples = [
+    ['dasAlTeLoate', new Audio("soundboard/resources/sounds/dasalteloate.mp3")],
+    ['oekNiWaar', new Audio("soundboard/resources/sounds/oekniwaar.mp3")],
+    ['eyKalmHeZalm', new Audio("soundboard/resources/sounds/eykalmhezalm.mp3")],
+    ['munt', new Audio("soundboard/resources/sounds/munt.mp3")],
+    ['ochMennekeToch', new Audio("soundboard/resources/sounds/ochmenneketoch.mp3")],
+    ['wegSfeer', new Audio("soundboard/resources/sounds/wegsfeer.mp3")],
+    ['gertruuuude', new Audio("soundboard/resources/sounds/gertruuuude.mp3")],
+    ['goeiendaag', new Audio("soundboard/resources/sounds/goeiendaag.mp3")],
+    ['kustNaMenKlte', new Audio("soundboard/resources/sounds/kustnamenkl.mp3")],
+    ['lapjepap', new Audio("soundboard/resources/sounds/lapjepap.mp3")],
+    ['lelijkIsNiks', new Audio("soundboard/resources/sounds/lelijkisniks.mp3")],
+    ['ochgodjemenas', new Audio("soundboard/resources/sounds/ochgodjemenas.mp3")]];
 
 document.addEventListener('DOMContentLoaded', e => {
-    document.getElementById('dasAlTeLoate').addEventListener("mousedown", () => dasAlTeLoate.play());
-    document.getElementById('oekNiWaar').addEventListener("mousedown", () => oekNiWaar.play());
-    document.getElementById('eyKalmHeZalm').addEventListener("mousedown", () => eyKalmHeZalm.play());
-    document.getElementById('munt').addEventListener("mousedown", () => munt.play());
-    document.getElementById('ochMennekeToch').addEventListener("mousedown", () => ochMennekeToch.play());
-    document.getElementById('wegSfeer').addEventListener("mousedown", () => wegSfeer.play());
-    document.getElementById('goeiendaag').addEventListener("mousedown", () => goeiendaag.play());
-    document.getElementById('ochgodjemenas').addEventListener("mousedown", () => ochgodjemenas.play());
-    document.getElementById('lapjepap').addEventListener("mousedown", () => lapjepap.play());
-    document.getElementById('lelijkIsNiks').addEventListener("mousedown", () => lelijkIsNiks.play());
-    document.getElementById('kustNaMenKlte').addEventListener("mousedown", () => kustNaMenKlte.play());
-    document.getElementById('gertruuuude').addEventListener("mousedown", () => gertruuuude.play());
+    soundTuples.forEach(st => document.getElementById(st[0]).addEventListener("mousedown", () => st[1].play()));
 });
