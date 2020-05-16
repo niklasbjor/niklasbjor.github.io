e06dc7973dfce4b1ec617e4097fde5dd059b482c
Niklas Björklund <niklas.bjor@gmail.com>
2020-05-16 10:16:50 +0200

    use event listeners to play sounds

--- soundboard/index_soundboard.js
+++ soundboard/index_soundboard.js
@@ -10,3 +10,18 @@ let kustNaMenKlte = new Audio("soundboard/resources/sounds/kustnamenkl.mp3");
 let lapjepap = new Audio("soundboard/resources/sounds/lapjepap.mp3");
 let lelijkIsNiks = new Audio("soundboard/resources/sounds/lelijkisniks.mp3");
 let ochgodjemenas = new Audio("soundboard/resources/sounds/ochgodjemenas.mp3");
+
+document.addEventListener('DOMContentLoaded', e => {
+    document.getElementById('dasAlTeLoate').addEventListener("mousedown", () => dasAlTeLoate.play());
+    document.getElementById('oekNiWaar').addEventListener("mousedown", () => oekNiWaar.play());
+    document.getElementById('eyKalmHeZalm').addEventListener("mousedown", () => eyKalmHeZalm.play());
+    document.getElementById('munt').addEventListener("mousedown", () => munt.play());
+    document.getElementById('ochMennekeToch').addEventListener("mousedown", () => ochMennekeToch.play());
+    document.getElementById('wegSfeer').addEventListener("mousedown", () => wegSfeer.play());
+    document.getElementById('goeiendaag').addEventListener("mousedown", () => goeiendaag.play());
+    document.getElementById('ochgodjemenas').addEventListener("mousedown", () => ochgodjemenas.play());
+    document.getElementById('lapjepap').addEventListener("mousedown", () => lapjepap.play());
+    document.getElementById('lelijkIsNiks').addEventListener("mousedown", () => lelijkIsNiks.play());
+    document.getElementById('kustNaMenKlte').addEventListener("mousedown", () => kustNaMenKlte.play());
+    document.getElementById('gertruuuude').addEventListener("mousedown", () => gertruuuude.play());
+});
