cd19399fdc507cb744bad1e72f97710776b5909b
Niklas Björklund <niklas.bjor@gmail.com>
2020-05-30 20:46:37 +0200

    small cleanup

--- soundboard/index_soundboard.js
+++ soundboard/index_soundboard.js
@@ -29,7 +29,6 @@ function handleClick(tuple) {
         document.getElementById(easterEggButton).disabled = true;
     } else {
         playSound(tuple);
-
         document.getElementById(easterEggButton).disabled = false;
     }
 }
