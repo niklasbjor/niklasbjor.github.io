977123558235bdb9d0710d05f98ef886d70cd1f0
Niklas Björklund <niklas.bjor@gmail.com>
2020-09-11 23:45:54 +0200

    move css to separate file

--- keeropkeer/index.js
+++ keeropkeer/index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
+app.use('/resources', express.static(path.join(__dirname, 'resources')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
